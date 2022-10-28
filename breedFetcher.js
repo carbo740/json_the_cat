f821e803a052371506f502b4dc151cc2b83c6157
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-10-28 15:46:46 +0000

    Refactored and added mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,24 @@
-let breed = process.argv.slice(2);
 const request = require("request");
-let apiSite = `https://api.thecatapi.com/v1/breeds/search?q=${breed[0]}`;
 
-request(apiSite, (error, response, body) => {
-  if (error) {
-    console.log("Error: ", error);
-    return;
-  }
-  const data = JSON.parse(body);
-  
-  if (data.length === 0) {
-    console.log("Breed not found. Please try another breed.");
-    return;
-  }
-  console.log(data[0].description);
 
-});
+const fetchBreedDescription = function(breed, callback) {
+  let apiSite = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(apiSite, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback("Please try another breed.", null)
+      return;
+    }
+    const desc = data[0].description;
+    callback(null, desc);
+  });
+};
+
+module.exports = { fetchBreedDescription };
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,26 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns a message that tells you to try another breed', (done) => {
+    fetchBreedDescription('HelloWorld', (err, desc) => {
+      // expect to see an error message about no breed name found
+      assert.equal(err, "Please try another breed.");
+      done();
+    });
+  });
+});
